65dfd55daa9f0c811c8eb7f46cbb420eaa103438
julianof03 <jlprod08@gmail.com>
2022-09-16 02:26:11 -0300

    chore: adjust code for deploy on heroku

--- src/index.js
+++ src/index.js
@@ -6,10 +6,10 @@ import signupRouter from "./routers/signup.routers.js";
 
 const app = express();
 app.use(cors());
-app.use(express.json());
-app.use(signinRouter)
+app.use(express.json()); 
+app.use(signinRouter) 
 app.use(signupRouter)
 
 
 
-app.listen(5000, () => console.log("Listening on port 5000"));
+app.listen(process.env.PORT, ()=> { console.log("Server running on port " + process.env.PORT); });
