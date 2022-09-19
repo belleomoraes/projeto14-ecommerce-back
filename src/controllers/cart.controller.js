9c95b8bcfd0f5fdbac62cca563ba4378eb29611f
belleomoraes <iolliveira.moraes@gmail.com>
2022-09-19 00:50:38 -0300

    feat get history

--- src/controllers/cart.controller.js
+++ src/controllers/cart.controller.js
@@ -1,16 +1,16 @@
 import db from "../database/db.js";
+import dayjs from "dayjs"
 
 async function AddCart(req, res) {
-    const { value, token, itens } = req.body;
-  
-    try {
-      await db.collection("cart").insertOne({ value, token, itens });
+  const { value, token, itens } = req.body;
 
-      res.sendStatus(201);
-    } catch (error) {
-      res.status(500).send({ message: error.message });
-    }
+  try {
+    await db.collection("cart").insertOne({ value, token, itens, date: dayjs().format("D/M") });
+    res.sendStatus(201);
+  } catch (error) {
+    res.status(500).send({ message: error.message });
   }
-  
+}
+
 
 export { AddCart };
--- src/controllers/history.controller.js
+++ src/controllers/history.controller.js
@@ -14,7 +14,7 @@ async function getHistory(req, res) {
       return res.status(401).send({ message: "O usuário não está logado" });
     }
     try {
-      const historyUser = await db.collection("history").find({ userId: session.userId }).toArray();
+      const historyUser = await db.collection("cart").find({ token }).toArray();
       res.send(historyUser);
     } catch (error) {
       res.status(500).send({ message: error.message });
